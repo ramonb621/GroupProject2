54bfc4d358e02352c6cc6ca5662a520c533a7677
Ramon Barajas <ramonb621@yahoo.com>
2019-09-28 12:43:23 -0700

    added api routes for businness, volunteers, services, and html file paths

---
+++ routes/business-apiRoutes.js
@@ -0,0 +1,51 @@
+var db = require("../models");
+
+module.exports = function(app) {
+  app.get("/api/businesses", function(req, res) {
+    db.Business.findAll({}).then(function(dbBusiness) {
+      res.json(dbBusiness);
+    });
+  });
+
+  app.post("/api/businesses", function(req, res) {
+    db.Business.create({
+      businessName: req.body.name,
+      contactName: req.body.contactName,
+      email: req.body.email,
+      location: req.body.location,
+      state: req.body.state,
+      city: req.body.city,
+      zipCode: req.body.zipCode
+    }).then(function(dbBusiness) {
+      res.json(dbBusiness);
+    });
+  });
+
+  app.put("/api/businesses", function(req, res) {
+    db.Business.update({
+      businessName: req.body.name,
+      contactName: req.body.contactName,
+      email: req.body.email,
+      location: req.body.location,
+      state: req.body.state,
+      city: req.body.city,
+      zipCode: req.body.zipCode
+    }, {
+      where: {
+        id: req.body.id
+      }
+    }).then(function(dbBusiness) {
+      res.json(dbBusiness);
+    });
+  });
+
+  app.delete("/api/businesses/:id", function(req, res) {
+    db.Business.destroy({ 
+      where: { 
+        id: req.params.id 
+      }
+    }).then(function(dbBusiness) {
+      res.json(dbBusiness);
+    });
+  });
+};
