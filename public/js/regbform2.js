d3064a43175daf68d6eae42473234642933e4d29
Marine Mekhakyan <marinemekhakyan@gmail.com>
2019-10-04 18:14:04 -0700

    add parsley validation

--- public/js/regbform2.js
+++ public/js/regbform2.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   $(document).on("submit", "#business-reg-form2", handleAuthorFormSubmit);
-// $("#business-reg-form2").on("submit", function (event) {});
+  // $("#business-reg-form2").on("submit", function (event) {});
 
   var foodD = $("#food-desc");
   var drinksD = $("#drinks-desc");
@@ -11,99 +11,98 @@ $(document).ready(function() {
 
   function handleAuthorFormSubmit(event) {
     event.preventDefault();
-        if (document.getElementById('Food').checked) {
-            if (!foodD.val().trim()) {
-                foodD.addClass("is-invalid");
-                event.preventDefault();
-            } else {
-                var food = {
-                    category: $("#Food").attr("value"),
-                    description: foodD.val().trim()
-                };
-                addToServ(food);
-            }
-        }
-        if (document.getElementById('Drinks').checked) {
-            if (!drinksD.val().trim()) {
-                drinksD.addClass("is-invalid");
-                event.preventDefault();
-            } else {
-                var drinks = {
-                    category: $("#Drinks").attr("value"),
-                    description: drinksD.val().trim()
-                };
-                addToServ(drinks);
-            }
-        }
-        if (document.getElementById('Shelter').checked) {
-            if (!shelterD.val().trim()) {
-                shelterD.addClass("is-invalid");
-                event.preventDefault();
-            } else {
-                var shelter = {
-                    category: $("#Shelter").attr("value"),
-                    description: shelterD.val().trim()
-                };
-                addToServ(shelter);
-            }
-        }
-        if (document.getElementById('Clothing').checked) {
-            if (!clothesD.val().trim()) {
-                clothesD.addClass("is-invalid");
-                event.preventDefault();
-            } else {
-                var clothing = {
-                    category: $("#Clothing").attr("value"),
-                    description: clothesD.val().trim()
-                };
-                addToServ(clothing);
-            }
-        }
-        if (document.getElementById('Transportation').checked) {
-            if (!transD.val().trim()) {
-                transD.addClass("is-invalid");
-                event.preventDefault();
-            } else {
-                var transportation = {
-                    category: $("#Transportation").attr("value"),
-                    description: transD.val().trim()
-                };
-                addToServ(transportation);
-            }
-        }
-        if (document.getElementById('Other').checked) {
-            if (!otherD.val().trim()) {
-                otherD.addClass("is-invalid");
-                event.preventDefault();
-            } else {
-                var other = {
-                    category: $("#Other").attr("value"),
-                    description: otherD.val().trim()
-                };
-                addToServ(other);
-            }
-        }
-        
-        $("#Food").val("");
-        $("#Drinks").val("");
-        $("#Shelter").val("");
-        $("#Clothing").val("");
-        $("#Transportation").val("");
-        $("#Other").val("");
-        
-        $("#regbform2").empty();
-        $("#regbform2").append(
-            "<p id='confirm'><b> We've received your form. Thank you!</b></p>"
-            );
-            $("#regbform2").append(
-                "<a href='/'><button class='btn btn-info' type='submit'>HOME</button></a>"
-                );
-            }
-            
-            function addToServ(serviceData) {
-                $.post("/api/services", serviceData).then(function (data) {
-                    console.log(data);
-                });
-            }
-            
-        });
+    if (document.getElementById("Food").checked) {
+      if (!foodD.val().trim()) {
+        foodD.addClass("is-invalid");
+        event.preventDefault();
+      } else {
+        var food = {
+          category: $("#Food").attr("value"),
+          description: foodD.val().trim()
+        };
+        addToServ(food);
+      }
+    }
+    if (document.getElementById("Drinks").checked) {
+      if (!drinksD.val().trim()) {
+        drinksD.addClass("is-invalid");
+        event.preventDefault();
+      } else {
+        var drinks = {
+          category: $("#Drinks").attr("value"),
+          description: drinksD.val().trim()
+        };
+        addToServ(drinks);
+      }
+    }
+    if (document.getElementById("Shelter").checked) {
+      if (!shelterD.val().trim()) {
+        shelterD.addClass("is-invalid");
+        event.preventDefault();
+      } else {
+        var shelter = {
+          category: $("#Shelter").attr("value"),
+          description: shelterD.val().trim()
+        };
+        addToServ(shelter);
+      }
+    }
+    if (document.getElementById("Clothing").checked) {
+      if (!clothesD.val().trim()) {
+        clothesD.addClass("is-invalid");
+        event.preventDefault();
+      } else {
+        var clothing = {
+          category: $("#Clothing").attr("value"),
+          description: clothesD.val().trim()
+        };
+        addToServ(clothing);
+      }
+    }
+    if (document.getElementById("Transportation").checked) {
+      if (!transD.val().trim()) {
+        transD.addClass("is-invalid");
+        event.preventDefault();
+      } else {
+        var transportation = {
+          category: $("#Transportation").attr("value"),
+          description: transD.val().trim()
+        };
+        addToServ(transportation);
+      }
+    }
+    if (document.getElementById("Other").checked) {
+      if (!otherD.val().trim()) {
+        otherD.addClass("is-invalid");
+        event.preventDefault();
+      } else {
+        var other = {
+          category: $("#Other").attr("value"),
+          description: otherD.val().trim()
+        };
+        addToServ(other);
+      }
+    }
+
+    $("#Food").val("");
+    $("#Drinks").val("");
+    $("#Shelter").val("");
+    $("#Clothing").val("");
+    $("#Transportation").val("");
+    $("#Other").val("");
+
+    $("#regbform2").empty();
+    $("#regbform2").append(
+      "<p id='confirm'><b> We've received your form. Thank you!</b></p>"
+    );
+    $("#regbform2").append(
+      "<a href='/'><button class='btn btn-info' type='submit'>HOME</button></a>"
+    );
+  }
+
+  function addToServ(serviceData) {
+    $.post("/api/services", serviceData).then(function (data) {
+      console.log(data);
+    });
+  }
+});
---
+++ public/js/service.js
@@ -0,0 +1,18 @@
+// When the page loads, grab all of our businesses
+$.get("/api/all", function(data) {
+  console.log(data);
+  if (data.length !== 0) {
+    for (var i = 0; i < data.length; i++) {
+      var row = $("<div>");
+      row.addClass("chirp");
+
+      row.append("<p>" + data[i].author + " chirped.. </p>");
+      row.append("<p>" + data[i].body + "</p>");
+      row.append(
+        "<p>At " + moment(data[i].created_at).format("h:mma on dddd") + "</p>"
+      );
+
+      $("#chirp-area").prepend(row);
+    }
+  }
+});
